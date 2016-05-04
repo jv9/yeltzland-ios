cadcc96bc332344e6e35a4f76b3d2acba3916c08
John Pollard <johnp@bravelocation.com>
2016-05-04 10:38:04 +0100

    Initial Commit

---
+++ yeltzland/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yeltzland
+//
+//  Created by John Pollard on 04/05/2016.
+//  Copyright © 2016 John Pollard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
