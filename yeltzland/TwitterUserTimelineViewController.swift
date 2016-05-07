9d6c70808308eff0950b1e704f58bb0489c3a564
John Pollard <johnp@bravelocation.com>
2016-05-07 10:11:33 +0100

    Added spinner for Twitter view

--- yeltzland/TwitterUserTimelineViewController.swift
+++ yeltzland/TwitterUserTimelineViewController.swift
@@ -15,6 +15,7 @@ import Font_Awesome_Swift
 class TwitterUserTimelineViewController: TWTRTimelineViewController, TWTRTweetViewDelegate, SFSafariViewControllerDelegate {
     
     var userScreenName: String!
+    var spinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,37 @@ class TwitterUserTimelineViewController: TWTRTimelineViewController, TWTRTweetVi
                 
         self.view.backgroundColor = AppColors.TwitterBackground
         self.tableView.separatorColor = AppColors.TwitterSeparator
+        self.showSpinner()
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (indexPath.row == tableView.indexPathsForVisibleRows!.last!.row) {
+            self.hideSpinner()
+        }
+    }
+    
+    func showSpinner() {
+        if (self.spinner != nil) {
+            self.hideSpinner()
+        }
+        
+        let topPosition = (self.navigationController?.navigationBar.frame.size.height)! + CGRectGetHeight(UIApplication.sharedApplication().statusBarFrame)
+        let tableViewHeight = CGRectGetHeight(view.frame) -
+            (topPosition + CGRectGetHeight((self.tabBarController?.tabBar.frame)!));
+        let overlayPosition = CGRectMake(0, self.view.bounds.origin.y, self.view.bounds.size.width, tableViewHeight)
+        
+        self.spinner = UIActivityIndicatorView(frame:overlayPosition)
+        self.spinner.color = AppColors.SpinnerColor
+        self.view.addSubview(self.spinner)
+        self.spinner.startAnimating()
+    }
+    
+    func hideSpinner() {
+        if (self.spinner != nil) {
+            self.spinner.stopAnimating()
+            self.spinner.removeFromSuperview()
+            self.spinner = nil;
+        }
     }
     
     func tweetView(tweetView: TWTRTweetView, didTapURL url: NSURL) {
