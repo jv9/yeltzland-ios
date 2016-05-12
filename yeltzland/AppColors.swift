5a8e64b811cfca6752181523b161e50ba5ace574
John Pollard <johnp@bravelocation.com>
2016-05-12 10:11:26 +0100

    Updated to use new club badge and blue hue

--- yeltzland/AppColors.swift
+++ yeltzland/AppColors.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-let YeltzBlueColor = UIColor(red: 6.0/255.0, green: 88.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+let YeltzBlueColor = UIColor(red: 6.0/255.0, green: 55.0/255.0, blue: 150.0/255.0, alpha: 1.0)
 let LightBlueColor = UIColor(red: 202.0/255.0, green: 220.0/255.0, blue: 235.0/255.0, alpha: 1.0)
 let FacebookBlueColor = UIColor(red: 71.0/255.0, green: 96.0/255.0, blue: 159.0/255.0, alpha: 1.0)
 let StourbridgeRedColor = UIColor(red: 158.0/255.0, green: 0.0/255.0, blue: 26.0/255.0, alpha: 1.0)
