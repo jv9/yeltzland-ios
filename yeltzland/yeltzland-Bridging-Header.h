94766544e844bead5bb0f6f2ec32b485f087b2eb
John Pollard <johnp@bravelocation.com>
2016-05-18 11:21:35 +0100

    Initial working notifications code

--- yeltzland/yeltzland-Bridging-Header.h
+++ yeltzland/yeltzland-Bridging-Header.h
@@ -1,13 +1,1 @@
-//
-//  yeltzland-Bridging-Header.h
-//  yeltzland
-//
-//  Created by John Pollard on 18/05/2016.
-//  Copyright © 2016 John Pollard. All rights reserved.
-//
-
-#ifndef yeltzland_Bridging_Header_h
-#define yeltzland_Bridging_Header_h
-
-
-#endif /* yeltzland_Bridging_Header_h */
+#import <Google/CloudMessaging.h>
