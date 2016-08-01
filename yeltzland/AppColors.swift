156cd38e199ee7ee22bf4fd685e74a405f70380f
John Pollard <johnp@bravelocation.com>
2016-08-01 09:40:33 +0100

    Darker blue screen for watch app

--- yeltzland/AppColors.swift
+++ yeltzland/AppColors.swift
@@ -12,6 +12,7 @@ import UIKit
 let YeltzBlueColor = UIColor(red: 6.0/255.0, green: 55.0/255.0, blue: 150.0/255.0, alpha: 1.0)
 let LightBlueColor = UIColor(red: 202.0/255.0, green: 220.0/255.0, blue: 235.0/255.0, alpha: 1.0)
 let LightBlueTransparentColor = UIColor(red: 202.0/255.0, green: 220.0/255.0, blue: 235.0/255.0, alpha: 0.8)
+let DarkBlueColor = UIColor(red: 6.0/255.0, green: 29.0/255.0, blue: 73.0/255.0, alpha: 1.0)
 let FacebookBlueColor = UIColor(red: 71.0/255.0, green: 96.0/255.0, blue: 159.0/255.0, alpha: 1.0)
 let StourbridgeRedColor = UIColor(red: 158.0/255.0, green: 0.0/255.0, blue: 26.0/255.0, alpha: 1.0)
 let EvostickRedColor = UIColor(red: 252.0/255.0, green: 0.0/255.0, blue: 6.0/255.0, alpha: 1.0)
@@ -88,6 +89,7 @@ class AppColors {
     static var WatchFixtureDraw = UIColor(red: 127.0/255.0, green: 127.0/255.0, blue: 255.0/255.0, alpha: 1.0)
     static var WatchFixtureLose = UIColor(red: 255.0/255.0, green: 127.0/255.0, blue: 127.0/255.0, alpha: 1.0)
     static var WatchComplicationColor:UIColor = LightBlueColor
+    static var WatchBackgroundColor:UIColor = DarkBlueColor
 }
 
     
